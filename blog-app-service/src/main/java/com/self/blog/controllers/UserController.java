2d3126ada4ae158f38d59baedf7460332c1d3254
sachinsshakya <sachingagra.shakya@gmail.com>
2023-07-25 15:43:36 +0530

    React Frontend Included

--- blog-app-service/src/main/java/com/self/blog/controllers/UserController.java
+++ blog-app-service/src/main/java/com/self/blog/controllers/UserController.java
@@ -6,14 +6,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.self.blog.dto.ApiResponse;
 import com.self.blog.dto.UserDto;
@@ -21,6 +14,7 @@ import com.self.blog.services.UserService;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin("*")
 public class UserController {
 
 	@Autowired
